f412400c67e874f2f4f2035a23ba965cd619878a
jiehuan <jiehuan@gmail.com>
2018-05-28 01:28:50 -0400

    Sun

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # insight_edgar
+
+# Approach
+
+The following dictionaries and lists are used for processing.
+
+    main = dict()		# key: IP, value: list[start-time, start unix time, end-time, end unix-time]
+    time_ip = dict()	# key: last active unix-time, value: list of IPs each in sequential orders
+    count = dict()  	# key: IP, value: set of IDs
+    active_time  = []   # list of end unix-time of live sessions
+    ip_order = []   	# list of live IPs with a sequential order
+	
+https://github.com/jiehuan/insight_edgar/image/flow.png
+
+
+# Requirements
+
+This solution is code in Python 3.6.5. Two packages need to be imported.
+
+	import sys
+	from dateutil.parser import parse
+	
+
+# Run Instructions
+
+execute 'run.sh' in terminal
+
+./run.sh
+
+
+## default file names and locations
+
+Python file:			./src/sessionization.py 
+Input csv file:			./input/log.csv 
+Inactivity period file:	./input/inactivity_period.txt 
+Output file:			./output/sessionization.txt
+
+If file names or locations need to be changed, the command in run.sh should also be changed accordingly.
--- src/sessionization.py
+++ src/sessionization.py
@@ -10,94 +10,110 @@ from dateutil.parser import parse
 
 def sessionization(filein, inactive, fileout):
 
-    inactivity_period = open(inactive, "r")
-    interval = int(inactivity_period.readline())
-    inactivity_period.close()
-    
+    try:
+        inactivity_period = open(inactive, "r")
+        interval = int(inactivity_period.readline())
+        inactivity_period.close()
+    except IOError:
+        print ('Cannot open inactivity_period file.')
+        return
+        
     w = open(fileout, "w")
     
-    main = dict()   # key: IP, value: list[start-time, start unix time, end-time, end unix-time]
+    main = dict()       # key: IP, value: list[start-time, start unix time, end-time, end unix-time]
     time_ip = dict()    # key: last active unix-time, value: list of IPs each in sequential orders
-    count = dict()  # key: IP, value: set of IDs
+    count = dict()      # key: IP, value: set of IDs
     active_time  = []   # list of end unix-time of live sessions
-    ip_order = []   # list of live IPs with a sequential order
+    ip_order = []       # list of live IPs with a sequential order
+    last_stamp = parse('1970-1-1').timestamp()
     
-    with open(filein, "r") as f:
+    try:   
+        with open(filein, "r") as f:
+            for i, line in enumerate(f):
+                if i>0:     # skip the header line
+                    try:
+                        s = line.split(',')
+                        ip = s[0]
+                        idt = s[4] + s[5] + s[6]    # use as identification for each request
+                    except:
+                        print ('Read line error. Line is skipped')
+                        continue
+                    try:
+                        time = s[1] +' '+ s[2]
+                        timestamp = parse(time).timestamp() # convert time to unix time
+                    except:
+                        print ('Cannot parse time. Line is skipped')
+                        continue
     
-        for i, line in enumerate(f):
-            if i>0: # skip the header line
-                s = line.split(',')
-                ip = s[0]
-                idt = s[4] + s[5] + s[6]
-                time = s[1] +' '+ s[2]
-                timestamp = parse(time).timestamp()
+                    # process expiring session
+                    if last_stamp != timestamp:
+                        last_stamp = timestamp
     
-                # process expiring session
-                if i==1:
-                    last_stamp = timestamp
-                    active_time.append(timestamp)
+                        # write expired sessions to fileout
+                        for j in range(len(active_time)-1, -1, -1):
+                            if (timestamp - active_time[j]) > interval:
     
-                if last_stamp != timestamp:
-                    last_stamp = timestamp
-                    for j in range(len(active_time)-1, -1, -1):
-                        if (timestamp - active_time[j]) > interval:
-                            expired_time = active_time[:j+1]
-                            active_time = active_time[j+1:]
-                            for k in range(len(expired_time)):
-                                ip_list = time_ip.get(expired_time[k])
-                               
-                                del time_ip[expired_time[k]]
-                               
-                                for m in range(len(ip_list)):
-                                    tmp = main[ip_list[m]]
-                                    t = str(int(tmp[3] - tmp[1]+1))
-                                    c = str(len(count[ip_list[m]]))
-                                    res_line = (ip_list[m] + ','+ tmp[0] + ',' + tmp[2] + ','+ t +','+ c)
-                                    w.write(res_line)
-                                    w.write('\n')
-                                    del main[ip_list[m]]
-                                    del count[ip_list[m]]
-                                    ip_order.remove(ip_list[m])
-                            
-                            del expired_time    
-                            break
-                    active_time.append(timestamp)
+                                expired_time = active_time[:j+1]        # remove expired timestamp from active_time
+                                active_time = active_time[j+1:]
+                                for k in range(len(expired_time)):
+                                    ip_list = time_ip[expired_time[k]]  # get the list of IPs at the expired timestamp
+    
+                                    del time_ip[expired_time[k]]        # del this timestamp from time_ip dict
+    
+                                    for m in range(len(ip_list)):       # write each IP to fileout
+                                        tmp = main[ip_list[m]]
+                                        t = str(int(tmp[3] - tmp[1]+1))
+                                        c = str(len(count[ip_list[m]]))
+                                        res_line = (ip_list[m] + ','+ tmp[0] + ',' + tmp[2] + ','+ t +','+ c)
+                                        w.write(res_line)
+                                        w.write('\n')
+                                        # del this IP in main, cout and ip_order
+                                        del main[ip_list[m]]
+                                        del count[ip_list[m]]
+                                        ip_order.remove(ip_list[m])
+    
+                                del expired_time
+                                break
+                        active_time.append(timestamp)   # add current timestamp to active_time list
     
     
-                # add new record
-                if timestamp not in time_ip:
-                    tmp_list1 = []
-                else:
-                    tmp_list1 = time_ip[timestamp]
-                if ip not in tmp_list1:
-                    tmp_list1.append(ip)
-                    time_ip[timestamp] = tmp_list1
-                            
-                if ip not in main:
-                    main[ip] = [time, timestamp, time, timestamp]
-                    count[ip] = set()
-                    ip_order.append(ip)
-                    
-                else:
-                    main_tmp = main[ip]
-                    tmp_list2 = time_ip[main_tmp[3]]
-
-                    if timestamp != main_tmp[3]:
-                        tmp_list2.remove(ip)
-                        if len(tmp_list2)==0:
-                            del time_ip[main_tmp[3]]
-                            active_time.remove(main_tmp[3])
-                        else:
-                            time_ip[main_tmp[3]] = tmp_list2
-                            
-                        
-                        main_tmp[2] = time
-                        main_tmp[3] = timestamp
-                        main[ip] = main_tmp
-                count[ip].add(idt)
-                
+                    # add new record
+                    if timestamp not in time_ip:    # add new timestamp of this request to time_ip dict
+                        tmp_list1 = []
+                    else:
+                        tmp_list1 = time_ip[timestamp]
     
-    #finish writing all un-expired sessions to file
+                    if ip not in tmp_list1:
+                        tmp_list1.append(ip)
+                        time_ip[timestamp] = tmp_list1
+    
+                    if ip not in main:              # add or update this request to main, ip_order, count
+                        main[ip] = [time, timestamp, time, timestamp]
+                        count[ip] = set()
+                        ip_order.append(ip)
+    
+                    else:
+                        main_tmp = main[ip]
+                        tmp_list2 = time_ip[main_tmp[3]]
+    
+                        if timestamp != main_tmp[3]:
+                            tmp_list2.remove(ip)
+                            if len(tmp_list2)==0:
+                                del time_ip[main_tmp[3]]
+                                active_time.remove(main_tmp[3])
+                            else:
+                                time_ip[main_tmp[3]] = tmp_list2
+    
+                            main_tmp[2] = time
+                            main_tmp[3] = timestamp
+                            main[ip] = main_tmp
+    
+                    count[ip].add(idt)          # add request identification to count dict
+    except IOError:
+        print ('Cannot read input file')
+        return
+    
+    #write all un-expired sessions to file
     for n in range(len(ip_order)):
         tmp = main[ip_order[n]]
         t = str(int(tmp[3] - tmp[1]+1))
@@ -105,15 +121,14 @@ def sessionization(filein, inactive, fileout):
         res_line = (ip_order[n] + ','+ tmp[0] + ',' + tmp[2] + ','+ t +','+ c)
         w.write(res_line)
         w.write('\n')
-    
+
     f.close()
     w.close()
-
 
 if __name__ == '__main__':
     filenames = sys.argv
     filein = filenames[1]
     inactive = filenames[2]
     fileout = filenames [3]
-    
-    sessionization(filein, inactive, fileout)    
+
+    sessionization(filein, inactive, fileout)
