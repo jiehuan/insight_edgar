f1bed658d175f48ddb48c815cd56cbcf6c6acad1
jiehuan <jiehuan@gmail.com>
2018-05-29 01:33:11 -0400

    Mon

--- README.md
+++ README.md
@@ -8,16 +8,16 @@ The following dictionaries and lists are used for processing.
     time_ip = dict()	# key: last active unix-time, value: list of IPs each in sequential orders
     count = dict()  	# key: IP, value: set of IDs
     active_time  = []   # list of end unix-time of live sessions
-    ip_order = []   	# list of live IPs with a sequential order
 	
 ![Alternate image text](https://github.com/jiehuan/insight_edgar/blob/master/image/flow.png)
 
 
 # Requirements
 
-This solution is code in Python 3.6.5. Two packages need to be imported.
+This solution is code in Python 3.6.5. The following packages need to be imported.
 
 	import sys
+	import pandas as pd
 	from dateutil.parser import parse
 	
 
--- src/sessionization.py
+++ src/sessionization.py
@@ -6,6 +6,7 @@ Created on Thu May 24 14:34:29 2018
 @author: jiehuanhuang
 """
 import sys
+import pandas as pd
 from dateutil.parser import parse
 
 def sessionization(filein, inactive, fileout):
@@ -24,7 +25,6 @@ def sessionization(filein, inactive, fileout):
     time_ip = dict()    # key: last active unix-time, value: list of IPs each in sequential orders
     count = dict()      # key: IP, value: set of IDs
     active_time  = []   # list of end unix-time of live sessions
-    ip_order = []       # list of live IPs with a sequential order
     last_stamp = parse('1970-1-1').timestamp()
     
     try:   
@@ -67,10 +67,9 @@ def sessionization(filein, inactive, fileout):
                                         res_line = (ip_list[m] + ','+ tmp[0] + ',' + tmp[2] + ','+ t +','+ c)
                                         w.write(res_line)
                                         w.write('\n')
-                                        # del this IP in main, cout and ip_order
+                                        # del this IP in main and count
                                         del main[ip_list[m]]
                                         del count[ip_list[m]]
-                                        ip_order.remove(ip_list[m])
     
                                 del expired_time
                                 break
@@ -87,10 +86,9 @@ def sessionization(filein, inactive, fileout):
                         tmp_list1.append(ip)
                         time_ip[timestamp] = tmp_list1
     
-                    if ip not in main:              # add or update this request to main, ip_order, count
-                        main[ip] = [time, timestamp, time, timestamp]
+                    if ip not in main:              # add or update this request to main and count
+                        main[ip] = [time, timestamp, time, timestamp, i]
                         count[ip] = set()
-                        ip_order.append(ip)
     
                     else:
                         main_tmp = main[ip]
@@ -113,18 +111,25 @@ def sessionization(filein, inactive, fileout):
         print ('Cannot read input file')
         return
     
-    #write all un-expired sessions to file
-    for n in range(len(ip_order)):
-        tmp = main[ip_order[n]]
-        t = str(int(tmp[3] - tmp[1]+1))
-        c = str(len(count[ip_order[n]]))
-        res_line = (ip_order[n] + ','+ tmp[0] + ',' + tmp[2] + ','+ t +','+ c)
-        w.write(res_line)
-        w.write('\n')
-
-    f.close()
     w.close()
 
+    #write all un-expired sessions to file
+    
+    df = pd.DataFrame(columns=['IP', 'start_time', 'end_time', 't', 'c', 'index'])
+
+    for item in main:
+        tmp = main[item]
+        t = str(int(tmp[3]-tmp[1]+1))
+        c = len(count[item])
+        df = df.append({'IP': item, 'start_time': tmp[0], 'end_time': tmp[2], 't': t, 'c': c, 'index': tmp[4]}, ignore_index=True)
+
+    df = df.set_index('IP')
+    df = df.sort_values(by='index')
+    df = df.drop('index', axis=1)
+
+    df.to_csv(fileout, header=False, mode = 'a')
+    
+    
 if __name__ == '__main__':
     filenames = sys.argv
     filein = filenames[1]
